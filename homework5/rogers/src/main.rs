b295ac1bb96b74d18f2fe28341bb010940882209
Sam Rogers <sjr@samrogers.org>
2018-03-07 20:04:52 -0500

    q1/q2 + tests

--- homework5/rogers/src/main.rs
+++ homework5/rogers/src/main.rs
@@ -1,3 +1,27 @@
+extern crate num;
+use num::Num;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn yourname() -> String {
+    "Sam Rogers".to_string()
+}
+
+fn a_plus_bx<N: Num>(a: N, b: N, c: N) -> N {
+    a + b * c
+}
+#[cfg(test)]
+mod tests {
+    use {yourname, a_plus_bx};
+    #[test]
+    fn question1() {
+        assert!(yourname() == "Sam Rogers".to_string())
+    }
+
+    #[test]
+    fn question2() {
+        assert!(a_plus_bx(1, 2, 3) == 7)
+    }
 }
